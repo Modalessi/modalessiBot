e1b75aaaefc928a7cb871d4b18ae5bae8ea30b71
mrbasel <mrbasel555@gmail.com>
2021-07-31 16:35:06 +0300

    refactor + fix simple issue + use env var for bot token instead of file

--- main.py
+++ main.py
@@ -1,39 +1,28 @@
-import logging
+import os
 import requests
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 
 # READ TOKEN
-
-with open('./token.txt', 'r') as tokenFile:
-    TOKEN = tokenFile.readline()
-
-
+TOKEN = os.environ["TOKEN"]
 updater = Updater(TOKEN)
-
-
 
 helpMSG = '''
 1. /twitter
 
 '''
 
-
-
-
 def downloadTwitter(link) :
     payload = {
         "url" : link,
         "ver" : 1306
     }
-    r = requests.post("https://tvdl-api.saif.dev", data = payload)
-    r = r.json()
+    r = requests.post("https://tvdl-api.saif.dev", data = payload).json()
     print(r)
     downloadUrl = r["high"]["downloadURL"]
     r = requests.get(downloadUrl)
     open("./twitterVideo.mp4", "wb").write(r.content)
-
 
 
 # send telegram message
@@ -41,19 +30,17 @@ def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'hello welcome to modalessiBot use /help to show commands')
-
 
 
 def help(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(helpMSG)
 
-
-    
+   
 def twitter(update: Update, context: CallbackContext) -> None:
     text = update.message.text
+    link = ""
     if "\n" in text:
         link = text.split("\n" )[1]
     
@@ -69,16 +56,13 @@ def twitter(update: Update, context: CallbackContext) -> None:
         update.message.reply_video(file)
         
         
-
-
-print("Bot is ready")
-
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('twitter', twitter))
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('help', help))
 
 updater.start_polling()
+print("Bot is ready")
 # updater.idle()
 
 
