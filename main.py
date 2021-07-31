03b7ce6eb83e4bb7d3a5a1310dd529581ef2af6d
mrbasel <mrbasel555@gmail.com>
2021-07-31 17:19:59 +0300

    add url trim feature

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import os
+import os, json
 import requests
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 
-# READ TOKEN
+# Telegram bot token
 TOKEN = os.environ["TOKEN"]
+# API key for utrim.xyz
+API_KEY = os.environ["API_KEY"]
+
 updater = Updater(TOKEN)
 
 helpMSG = '''
 1. /twitter
+2. /trim
 
 '''
 
@@ -24,6 +28,24 @@ def downloadTwitter(link) :
     r = requests.get(downloadUrl)
     open("./twitterVideo.mp4", "wb").write(r.content)
 
+def trimLink(link: str) -> str:
+    if not API_KEY:
+        return ""
+
+    payload = json.dumps({
+    "url": link
+    })
+    headers = {
+    'x-api-key': '46dd21d0-fe89-4b15-9af7-22e6fcf575d5',
+    'Content-Type': 'application/json'
+    }
+    url = "https://utrim.xyz/api/trim"
+    r = requests.request("POST", url, headers=headers, data=payload)
+    
+    if r.status_code == 200:
+        return r.json()["data"]["link"]
+    return ""
+    
 
 # send telegram message
 def hello(update: Update, context: CallbackContext) -> None:
@@ -54,10 +76,24 @@ def twitter(update: Update, context: CallbackContext) -> None:
     # send file to telegram
     with open("./twitterVideo.mp4", "rb") as file:
         update.message.reply_video(file)
+
+
+def trim(update: Update, context: CallbackContext) -> None:
+    msg = update.message.text
+    if msg == "/trim":
+        update.message.reply_text("please send the link with the command ex: \n /trim PUT_YOUR_LINK_HERE")
+    else:
+        link = msg.split()[1]
+        if "\n" in msg:
+            link = msg.split("\n")[1]
         
+        trimmedLink = trimLink(link)
+        update.message.reply_text(trimmedLink or "Sorry, something went wrong")
+
         
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(CommandHandler('twitter', twitter))
+updater.dispatcher.add_handler(CommandHandler('trim', trim))
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('help', help))
 
